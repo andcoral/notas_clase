2c11016942d279cc2b504154ed693b13a2120a65
Andromeda Coral G <andcoral@lcg.unam.mx>
2025-04-01 23:05:50 -0600

    Se creo el script string.py para ver a for trabajando con el objeto iterable string.

---
+++ ciclos_210325/string.py
@@ -0,0 +1,6 @@
+# El ciclo for recorre caracter por caracter del string
+
+secuencia = "GATTCACTAG"
+
+for nucleotido in secuencia: # Despues del "for" ira el nombre de una variable (nucleotido) que se le asignara a cada posicion que se esta valorando. Despues del "in" va el objeto iterable que se recorrera (en este caso la cadena/string "secuencia")
+    print(nucleotido)
