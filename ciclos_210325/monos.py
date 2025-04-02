cc384890d59801bc72d76c8a45032fd1a2303950
Andromeda Coral G <andcoral@lcg.unam.mx>
2025-04-01 23:27:33 -0600

    Nos deshicimos de variables innecesarias en monos.py.

--- ciclos_210325/monos.py
+++ ciclos_210325/monos.py
@@ -3,8 +3,5 @@
 apes = ["Pongo pygmaeus", "Pan troglodytes", "Gorilla gorilla"]
 
 for ape in apes: # Pasa por cada elemento de la lista
-    name_length = len(ape) # Funcion len() que cuenta el numero de componentes de la string guardada en la posicion de la celda que estemos.
-    first_letter = ape[0] # Asignamos a la letra en la primera posicion (la cero) de la string en la celda que estemos.
-    
     # Usamos f-strings para tener valores de variables en la impresion.
-    print(f"{ape} is an ape. Its name starts with '{first_letter}'.\nIts name has {name_length} letters")
+    print(f"{ape} is an ape. Its name starts with '{ape[0]}'.\nIts name has {len(ape)} letters")
