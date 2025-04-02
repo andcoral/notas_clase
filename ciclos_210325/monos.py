dc7159e3fc176568ab3c70fc28cfe674de2face8
Andromeda Coral G <andcoral@lcg.unam.mx>
2025-04-01 23:24:43 -0600

    Se corrigio el archivo monos.py con indicaciones mas simples gracias a las f-strings.

--- ciclos_210325/monos.py
+++ ciclos_210325/monos.py
@@ -6,6 +6,5 @@ for ape in apes: # Pasa por cada elemento de la lista
     name_length = len(ape) # Funcion len() que cuenta el numero de componentes de la string guardada en la posicion de la celda que estemos.
     first_letter = ape[0] # Asignamos a la letra en la primera posicion (la cero) de la string en la celda que estemos.
     
-    # La forma en que se usa el print a continuacion es la de una version antigua y no tan amigable.
-    print(ape + " is an ape. Its name starts with " + first_letter)
-    print("Its name has " + str(name_length) + " letters") # Se agrego lo de "str" antes de la variable "length_name" porque el "+" usado es para concatenar UNICAMENTE string, y no int como es nuestra variable.
+    # Usamos f-strings para tener valores de variables en la impresion.
+    print(f"{ape} is an ape. Its name starts with '{first_letter}'.\nIts name has {name_length} letters")
