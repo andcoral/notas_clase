757dc356b788f874493995171c6b2105de612d16
Andromeda Coral G <andcoral@lcg.unam.mx>
2025-04-02 22:50:53 -0600

    Se termino el programa que arroja los tamaños de genes de un archivo gff.

--- ciclos_210325/tamano.py
+++ ciclos_210325/tamano.py
@@ -3,4 +3,7 @@
 with open("genes.gff") as file:
     for linea in file: # Usamos ciclo for porque los archivos son iterables, en estos cada unidad pasada son lineas.
         columnas = linea.strip().split("\t") # Con '.strip()' quitamos todos los espacios en blanco al principio y final de las lineas (NO de en medio). Con '.split()' dividimos cada que se encuentre un tabulador y se guarda el contenido como un elemento de la lista "columna".
-        print(columnas[3])
+        if columnas[3].isdigit() and columnas[4].isdigit(): # Condicion para cersiorarnos de que los datos ingresados son numericos.
+            tamano = int(columnas[4])-int(columnas[3])+1 # Funciones int() para volver esos valores de str a int. El "+1" es para no omitir un nucleotido.
+
+            print(f"Gen de tamaño: {tamano} pb")
