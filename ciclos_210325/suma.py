9cf5bf359e9d56cbd18583208866b152e170bbca
Andromeda Coral G <andcoral@lcg.unam.mx>
2025-04-02 19:55:47 -0600

    Se convirtieron los tipos de datos de la lista de str a int para que pudiesen operarse con sum().

--- ciclos_210325/suma.py
+++ ciclos_210325/suma.py
@@ -1,7 +1,7 @@
-# Usando la funcion input(), ademas del metodo .split() y 
+# Usando la funcion input(), ademas del metodo .split() y la funcion map() que convierte tipos de datos.
 
 numeros_str = input("Dame 3 numeros separados por espacio\n").split() # Todo lo que se reciba por input sera huardado en la string especificada.
  # La funcion al final del input toma al string recien creado y lo separa por espacios, convirtiendo al contenido en una lista
-print(numeros_str)
-suma = sum(numeros_str)
+lista_numeros = list(map(int,numeros_str)) # GUardamos en una lista los datos convertidos del tipo string a int para poder aplicarles sum()
+suma = sum(lista_numeros) # Funcion sum()
 print(suma)
