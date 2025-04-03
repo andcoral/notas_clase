a54985f869584e2e5fb122ed8cc5f432153c43c3
Andromeda Coral G <andcoral@lcg.unam.mx>
2025-04-02 20:04:47 -0600

    Se borro una funcion list() innecesaria del codigo.

--- ciclos_210325/suma.py
+++ ciclos_210325/suma.py
@@ -2,6 +2,7 @@
 
 numeros_str = input("Dame 3 numeros separados por espacio\n").split() # Todo lo que se reciba por input sera huardado en la string especificada.
  # La funcion al final del input toma al string recien creado y lo separa por espacios, convirtiendo al contenido en una lista
-lista_numeros = list(map(int,numeros_str)) # GUardamos en una lista los datos convertidos del tipo string a int para poder aplicarles sum()
+lista_numeros = map(int,numeros_str) # GUardamos en una lista los datos convertidos del tipo string a int para poder aplicarles sum()
+print(lista_numeros) # Descubrimos que el objeto map tambien es un iterable, porque sum() pudo trabajar con el.
 suma = sum(lista_numeros) # Funcion sum()
 print(suma)
