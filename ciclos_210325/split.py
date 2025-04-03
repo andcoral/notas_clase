8c9ec2e94dd2dfc2f24dc5971a52ef8b31584a3e
Andromeda Coral G <andcoral@lcg.unam.mx>
2025-04-02 19:26:18 -0600

    Se volvio a cambiar al separador, ademas de que se establecio un limite de accion.

--- ciclos_210325/split.py
+++ ciclos_210325/split.py
@@ -1,5 +1,5 @@
 # Recordando el uso del metodo .split()
 
-texto = "ATG,CGT,TAA,GGC"
-lista = texto.split(",") # ESpecificamos a la coma como separador
+texto = "ATG-CGT-TAA-GGC"
+lista = texto.split("-", 2) # ESpecificamos a la coma como separador
 print(lista)
