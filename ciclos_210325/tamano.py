5b8d13b4decac90fda59c8e477c46d1e2776e33f
Andromeda Coral G <andcoral@lcg.unam.mx>
2025-04-02 22:32:43 -0600

    Comprobacion de que se accede a los datos de interes del archivo gff.

--- ciclos_210325/tamano.py
+++ ciclos_210325/tamano.py
@@ -2,5 +2,5 @@
 
 with open("genes.gff") as file:
     for linea in file: # Usamos ciclo for porque los archivos son iterables, en estos cada unidad pasada son lineas.
-        print(linea)
-
+        columnas = linea.strip().split("\t") # Con '.strip()' quitamos todos los espacios en blanco al principio y final de las lineas (NO de en medio). Con '.split()' dividimos cada que se encuentre un tabulador y se guarda el contenido como un elemento de la lista "columna".
+        print(columnas[3])
