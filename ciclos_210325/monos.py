180fb926c374beea2dc53daf313b090325fb0578
Andromeda Coral G <andcoral@lcg.unam.mx>
2025-04-02 18:46:21 -0600

    Correccion de sintaxis archivo monos.py

--- ciclos_210325/monos.py
+++ ciclos_210325/monos.py
@@ -4,4 +4,4 @@ apes = ["Pongo pygmaeus", "Pan troglodytes", "Gorilla gorilla"]
 
 for ape in apes: # Pasa por cada elemento de la lista
     # Usamos f-strings para tener valores de variables en la impresion.
-    print(f"{ape} is an ape. Its name starts with '{ape[0]}'.\nIts name has {len(ape)} letters")
+    print(f"{ape} is an ape. Its name starts with '{ape[0]}'.\nIts name has {len(ape)} letters.")
