75baa4f31c8f119b9aec85a10909f857e8b760db
Andromeda Coral G <andcoral@lcg.unam.mx>
2025-04-02 23:41:58 -0600

    Se agrego explicacion y uso de funcion zip().

--- ciclos_210325/funciones.py
+++ ciclos_210325/funciones.py
@@ -4,3 +4,16 @@
 secuencia = "ATGCGT"
 for i, base in enumerate(secuencia): # 2 valores despues del for, por los 2 que regresa la funcion: índice, valor
     print(f"Posición {i}: {base}")
+
+
+# zip(), itera sobre multiples listas a la vez
+bases = "ATGC"
+complementos = "TACG"
+for base, complemento in zip(bases, complementos): # Van los 2 iteradores despues del in. Num de iterables = Num de variables 
+    print(f"{base} → {complemento}")
+
+# Cuando los iterables NO son del mismo tamano, la funcion zip() simplemente los compara al tamano del mas chico.
+genes = ["thrL", "thrA", "thrB"]
+longitudes = [66, 2463]  # Lista más corta
+for gen, tamaño in zip(genes, longitudes):
+    print(f"Gen: {gen}, Tamaño: {tamaño} pb")
