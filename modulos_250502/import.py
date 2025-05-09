4f97ad81bca3d6feed1955ad9ca7d54bcc575ea6
Andromeda Coral G <andcoral@lcg.unam.mx>
2025-05-09 09:39:27 -0600

    Primer programa de la clase del 02/Mayo/2025.

---
+++ modulos_250502/import.py
@@ -0,0 +1,7 @@
+import math # Importa todo el modulo. Es de biblioteca estándar
+
+import numpy as np # Despues de math por jerarquia (terceros). Uso con alias
+
+print(math.sqrt(16))
+
+arreglo = np.array([1,2,3]) # Uso de funcion (.array) de la libreria
